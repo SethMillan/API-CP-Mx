edcf8badf5c59c584f0b7b0b6d5d5fe8b37eaf88
Seth Millan <millanseth@gmail.com>
2023-12-20 22:58:00 -0600

    Controlador agregado

--- gestorCodigosPostales/src/main/java/com/millanseth/model/dao/EstadoDAO.java
+++ gestorCodigosPostales/src/main/java/com/millanseth/model/dao/EstadoDAO.java
@@ -5,6 +5,7 @@ import io.micronaut.data.repository.CrudRepository;
 //ocupamos el crud repository si tenemos poquillos datos, aqui son bastantes
 //entonces mejor que sea paginado jeje
 import io.micronaut.data.repository.PageableRepository;
+import jakarta.inject.Singleton;
 
 public interface EstadoDAO extends CrudRepository<Estado, Integer> {
 
