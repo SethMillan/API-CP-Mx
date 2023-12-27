e20446eb0e7e460e6a1c1f3ad2da92f13f4731a1
Seth Millan <millanseth@gmail.com>
2023-12-26 22:48:47 -0600

    Cambios al MunicipioIMPL

--- src/main/java/com/millanseth/service/impl/MunicipioImpl.java
+++ src/main/java/com/millanseth/service/impl/MunicipioImpl.java
@@ -1,4 +1,23 @@
 package com.millanseth.service.impl;
 
-public class MunicipioImpl {
+import com.millanseth.model.dao.MunicipioDAO;
+import com.millanseth.model.entity.Municipio;
+import com.millanseth.service.IMunicipio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class MunicipioImpl implements IMunicipio {
+    @Autowired
+    private MunicipioDAO municipioDAO;
+    @Override
+    public List<Municipio> listAll() {
+        return (List)municipioDAO.findAll();
+    }
+
+    @Override
+    public Municipio findById(Integer id) {
+        return municipioDAO.findById(id).orElse(null);
+    }
 }
