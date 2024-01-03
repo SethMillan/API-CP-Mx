bc35f8221ecfda6b76e9f5c0a3f847c1f6263920
Seth Millan <millanseth@gmail.com>
2024-01-02 20:10:52 -0600

    Se agrego las entidades CP y Asenta

---
+++ src/main/java/com/millanseth/model/entity/Asentamiento.java
@@ -0,0 +1,22 @@
+package com.millanseth.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+@Entity
+@Table(name = "tablaasenta")
+public class Asentamiento {
+    @Id
+    @Column(name="Asenta")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String Asenta;
+    @ManyToOne
+    @JoinColumn(name="CP")
+    private CodigoPostal codigoPostal;
+}
---
+++ src/main/java/com/millanseth/model/entity/CodigoPostal.java
@@ -0,0 +1,21 @@
+package com.millanseth.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+@Entity
+@Table(name="tablacp")
+public class CodigoPostal {
+    @Id
+    @Column(name="CP")
+    private Integer cp;
+    @ManyToOne
+    @JoinColumn(name="id_Mcpio")
+    private Municipio municipio;
+}
